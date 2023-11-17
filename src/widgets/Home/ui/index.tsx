56a3f5bf671bb4a54c274e9e15b55d6b41eb8bf3
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-17 18:46:36 +0600

    Version 1.0.3

--- src/widgets/Home/ui/index.tsx
+++ src/widgets/Home/ui/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Slide } from "react-awesome-reveal";
 
 export const HomeScreen = () => {
   return (
     <main className="container">
-      <h1>Special Solutions</h1>
-      <h2 className=" mt-4">Ваш Партнер в Строительстве</h2>
+      <Slide direction="left">
+        <h1 className="bigger text-custom-orange">Special Solutions</h1>
+      </Slide>
+      <Slide direction="right">
+        <h2 className="mt-4">Ваш Партнер в Строительстве</h2>
+      </Slide>
+      <Slide direction="left">
+        <p className="paragraph text-center mt-4">
+          <span className="orange">Special Solutions</span> представляет
+          портфолио передовых строительных работ. Мы стремимся к инновациям в
+          каждом проекте, создавая функциональные и эстетически привлекательные
+          объекты для наших клиентов.
+        </p>
+      </Slide>
     </main>
   );
 };
