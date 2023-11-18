5cdd87df84ba567b1a1e4a16e7bb2a6bcaad962b
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-18 18:21:18 +0600

    Version 1.1.5

--- src/features/Header/ui/index.tsx
+++ src/features/Header/ui/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useOpenMenu } from "@shared/lib/hooks/useOpenMenu";
 
