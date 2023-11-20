383c7d8abec843d24befcf4bbcfa39a9b7dc2571
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-20 16:31:29 +0600

    Version 1.2.2

--- src/shared/ui/Feature/index.tsx
+++ src/shared/ui/Feature/index.tsx
@@ -12,7 +12,9 @@ export const Feature: React.FC<FeatureProps> = ({
   marginTop,
 }) => {
   return (
-    <div className={`feature flex flex-col items-center ${marginTop}`}>
+    <div
+      className={`feature flex ${marginTop} max-[1024px]:flex-col items-center min-[1024px]:flex-row`}
+    >
       <span className="feature__number">{number}</span>
       <span className="feature__text text-center">{text}</span>
     </div>
--- src/widgets/Home/ui/AboutScreen/index.tsx
+++ src/widgets/Home/ui/AboutScreen/index.tsx
@@ -9,58 +9,82 @@ import "./styles.scss";
 export const AboutScreen = () => {
   return (
     <>
-      <Slide direction="left" triggerOnce={true}>
-        <h2 className="mt-12">О компании</h2>
-      </Slide>
-      <Slide direction="right" triggerOnce={true}>
-        <p className="paragraph text-center mt-8">
+      <section className="mobile-screen w-full flex flex-col items-center min-[1024px]:hidden">
+        <Slide direction="left" triggerOnce={true}>
+          <h2 className="mt-12">О компании</h2>
+        </Slide>
+        <Slide direction="right" triggerOnce={true}>
+          <p className="paragraph text-center mt-8">
+            Компания <span className="orange">Special Solution</span> занимается
+            разработкой передовых решений, способных удовлетворить самые
+            изысканные и специфические запросы наших клиентов. Мы сочетаем
+            глубокие знания отрасли с инновационным мышлением, чтобы предлагать
+            продукты и услуги, выходящие за рамки стандартных предложений.
+          </p>
+        </Slide>
+        <Slide direction="left" triggerOnce={true}>
+          <Feature
+            number="01"
+            text="Архитектурное Проектирование"
+            marginTop="mt-6"
+          />
+        </Slide>
+        <Slide direction="right" triggerOnce={true}>
+          <Feature number="02" text="Строительство" marginTop="mt-6" />
+        </Slide>
+        <Slide direction="left" triggerOnce={true}>
+          <Feature number="03" text="Фасадное остекление" marginTop="mt-6" />
+        </Slide>
+        <Slide direction="right" triggerOnce={true}>
+          <Feature
+            number="04"
+            text="Изготовление и монтаж офисных перегородок"
+            marginTop="mt-6"
+          />
+        </Slide>
+        <Slide direction="left" triggerOnce={true}>
+          <img src={Image} className="about-image mt-12" alt="about-image" />
+        </Slide>
+        <Slide direction="right" triggerOnce={true}>
+          <p className="paragraph text-center mt-8">
+            Компания <span className="orange">Special Solution</span> занимается
+            разработкой передовых решений, способных удовлетворить самые
+            изысканные и специфические запросы наших клиентов. Мы сочетаем
+            глубокие знания отрасли с инновационным мышлением, чтобы предлагать
+            продукты и услуги, выходящие за рамки стандартных предложений.
+          </p>
+        </Slide>
+        <Slide direction="left" triggerOnce={true}>
+          <Button
+            text="Связаться нами"
+            marginTop="mt-8"
+            type="outline"
+            onClick={() => console.log("holla")}
+          />
+        </Slide>
+      </section>
+      <section className="pc-screen w-full max-[1024px]:hidden">
+        <h2>О компании</h2>
+        <p className="paragraph mt-8 w-[60%]">
           Компания <span className="orange">Special Solution</span> занимается
           разработкой передовых решений, способных удовлетворить самые
           изысканные и специфические запросы наших клиентов. Мы сочетаем
           глубокие знания отрасли с инновационным мышлением, чтобы предлагать
           продукты и услуги, выходящие за рамки стандартных предложений.
         </p>
-      </Slide>
-      <Slide direction="left" triggerOnce={true}>
-        <Feature
-          number="01"
-          text="Архитектурное Проектирование"
-          marginTop="mt-6"
-        />
-      </Slide>
-      <Slide direction="right" triggerOnce={true}>
-        <Feature number="02" text="Строительство" marginTop="mt-6" />
-      </Slide>
-      <Slide direction="left" triggerOnce={true}>
-        <Feature number="03" text="Фасадное остекление" marginTop="mt-6" />
-      </Slide>
-      <Slide direction="right" triggerOnce={true}>
-        <Feature
-          number="04"
-          text="Изготовление и монтаж офисных перегородок"
-          marginTop="mt-6"
-        />
-      </Slide>
-      <Slide direction="left" triggerOnce={true}>
-        <img src={Image} className="about-image mt-12" alt="about-image" />
-      </Slide>
-      <Slide direction="right" triggerOnce={true}>
-        <p className="paragraph text-center mt-8">
-          Компания <span className="orange">Special Solution</span> занимается
-          разработкой передовых решений, способных удовлетворить самые
-          изысканные и специфические запросы наших клиентов. Мы сочетаем
-          глубокие знания отрасли с инновационным мышлением, чтобы предлагать
-          продукты и услуги, выходящие за рамки стандартных предложений.
-        </p>
-      </Slide>
-      <Slide direction="left" triggerOnce={true}>
-        <Button
-          text="Связаться нами"
-          marginTop="mt-8"
-          type="outline"
-          onClick={() => console.log("holla")}
-        />
-      </Slide>
+        <div className="about-content w-full flex justify-between mt-12">
+          <img src={Image} className="about-content__image" alt="about-image" />
+          <div className="about-content__text-container flex flex-col items-start">
+            <Feature number="01" text="Архитектурное  Проектирование" />
+            <Feature number="02" text="Строительство" />
+            <Feature number="03" text="Фасадное остекление" />
+            <Feature
+              number="04"
+              text="Изготовление и монтаж офисных перегородок"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
