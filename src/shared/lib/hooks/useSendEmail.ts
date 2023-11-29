265fc5dd23fcddb814ee802fe6fc22c8271fdc49
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-30 03:02:53 +0600

    Email-Error

--- src/shared/lib/hooks/useSendEmail.ts
+++ src/shared/lib/hooks/useSendEmail.ts
@@ -1,37 +1,49 @@
+// useSendEmail.ts
 import { useForm } from "react-hook-form";
-import emailjs from "@emailjs/browser";
+import emailjs from "emailjs-com";
 
 export interface SendFormData {
   full_name: string;
   phone_number: string;
   email: string;
-  option: string;
+  service_option: string;
 }
 
 export function useSendEmail() {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isSubmitting, isValid },
+    getValues, // This will be used to get form values
   } = useForm<SendFormData>({
     mode: "onChange",
-    criteriaMode: "all",
   });
 
-  const onSubmit = (data: SendFormData) => {
-    const formData = data as unknown as Record<string, unknown>;
+  const onSubmit = handleSubmit((formData) => {
+    const form = document.querySelector("form") as HTMLFormElement; // Assuming there's only one form on the page
 
     emailjs
-      .send("your_service_id", "your_template_id", formData, "your_user_id")
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID!,
+        process.env.REACT_APP_TEMPLATE_ID!,
+        form,
+        process.env.REACT_APP_EMAILJS_KEY!
+      )
+      .then((res: unknown) => {
+        console.log("send");
+        reset(); // Reset the form fields after successful submission
+      })
+      .catch((err: unknown) => console.error(err));
+  });
 
-  return { register, handleSubmit, onSubmit, errors, isSubmitting, isValid };
+  // If you need to do something with the form data before sending, use getValues() here
+
+  return {
+    register,
+    onSubmit, // No need to wrap handleSubmit here again
+    errors,
+    isSubmitting,
+    isValid,
+  };
 }
