eb2e4aec7548d4b858a36d24109123694455c80b
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-11 14:28:43 +0600

    Version 1.9.5

--- src/pages/Clients/ui/index.tsx
+++ src/pages/Clients/ui/index.tsx
@@ -4,6 +4,7 @@ import { Menu } from "@features/Menu/ui";
 import { useSelector } from "react-redux";
 import { RootState } from "@shared/lib/redux/store";
 import { ClientScreen } from "@widgets/Clients/ui";
+import { Footer } from "@features/Footer/ui";
 
 export const ClientsPage = () => {
   const isMenuOpen = useSelector((state: RootState) => state.menu.isOpen);
@@ -13,6 +14,7 @@ export const ClientsPage = () => {
       <Header />
       {isMenuOpen && <Menu />}
       <ClientScreen />
+      <Footer />
     </>
   );
 };
--- src/widgets/Clients/ui/index.tsx
+++ src/widgets/Clients/ui/index.tsx
@@ -1,4 +1,5 @@
 import { Gallery } from "@features/Gallery/ui";
+import { Fade, Slide } from "react-awesome-reveal";
 
 import logo1 from "@assets/clients/31.svg";
 import logo2 from "@assets/clients/apple_city.svg";
@@ -10,20 +11,86 @@ import logo7 from "@assets/clients/lenovo.svg";
 import logo8 from "@assets/clients/siemes.svg";
 import logo9 from "@assets/clients/syngenta.svg";
 
+import "./styles.scss";
+
 export const ClientScreen = () => {
+  const galleryItems = [
+    {
+      image: logo1,
+    },
+    {
+      image: logo2,
+    },
+    {
+      image: logo3,
+    },
+    {
+      image: logo4,
+    },
+    {
+      image: logo5,
+    },
+    {
+      image: logo6,
+    },
+    {
+      image: logo7,
+    },
+    {
+      image: logo8,
+    },
+    {
+      image: logo9,
+    },
+  ];
+
   return (
     <>
       <main className="content-container">
-        <h1>Наши клиенты</h1>
-        <p className="paragraph mt-4 text-center w-[100%] min-[1024px]:w-[70%]">
-          В мире, где каждое пространство имеет значение, компания Special
-          Solutions выступает в качестве ключевого игрока в индустрии создания
-          идеальных бизнес-пространств. С момента своего основания, мы
-          зарекомендовали себя как лидера в области инновационного и
-          функционального строительства, предназначенного для бизнеса всех
-          масштабов.
-        </p>
-        <Gallery image={logo1} image2={logo2} image3={logo3} image4={logo4} />
+        <Slide triggerOnce>
+          <h1>Наши клиенты</h1>
+        </Slide>
+        <Slide
+          direction="right"
+          className="w-full flex justify-center"
+          delay={200}
+          triggerOnce
+        >
+          <p className="paragraph mt-4 text-center w-[100%] min-[1024px]:w-[70%]">
+            В мире, где каждое пространство имеет значение, компания Special
+            Solutions выступает в качестве ключевого игрока в индустрии создания
+            идеальных бизнес-пространств. С момента своего основания, мы
+            зарекомендовали себя как лидера в области инновационного и
+            функционального строительства, предназначенного для бизнеса всех
+            масштабов.
+          </p>
+        </Slide>
+        <div className="w-full mt-2 mb-4 min-[1024px]:hidden">
+          <Gallery items={galleryItems} />
+        </div>
+        <div className="hidden lg:flex w-full mt-16 flex-col items-center">
+          <div className="w-[70%] flex items-center justify-between">
+            <Fade cascade triggerOnce>
+              <img src={logo1} className="logo thirty-one" alt="logotype" />
+              <img src={logo2} className="logo apple-city" alt="logotype" />
+              <img src={logo3} className="logo burger-king" alt="logotype" />
+            </Fade>
+          </div>
+          <div className="w-[70%] flex items-center justify-between mt-20">
+            <Fade cascade delay={1000} triggerOnce>
+              <img src={logo4} className="logo freedom" alt="logotype" />
+              <img src={logo5} className="logo kaspi" alt="logotype" />
+              <img src={logo6} className="logo lampadario" alt="logotype" />
+            </Fade>
+          </div>
+          <div className="w-[70%] flex items-center justify-between mt-20">
+            <Fade cascade delay={1500} triggerOnce>
+              <img src={logo7} className="logo lenovo" alt="logotype" />
+              <img src={logo8} className="logo siemens" alt="logotype" />
+              <img src={logo9} className="logo syngenta" alt="logotype" />
+            </Fade>
+          </div>
+        </div>
       </main>
       ;
     </>
